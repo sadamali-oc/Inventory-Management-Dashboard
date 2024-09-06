f96ac9d9a469b561be5012e00fd3e623cdc1eae1
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-09-06 05:46:15 +0530

    installed the packages

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+     Hello
+     
+      </main>
+     
+  );
+}
